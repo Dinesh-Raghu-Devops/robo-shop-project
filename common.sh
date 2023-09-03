1720e0d9efea3e54353acee19bd9545b37a0c012
dineshdevops <dbramayanam@gmail.com>
2023-09-03 15:37:49 +0530

    Corrected errors

--- common.sh
+++ common.sh
@@ -1,5 +1,11 @@
 log=/tmp/logfile
 func_nodeJS(){
+  echo -e "\e[33m<<<<<<Copying mongo repos to yum.repos.d>>>>>>\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  echo $?
+  echo -e "\e[33m<<<<<<Copying ${component} file to systemd>>>>>>\e[0m"
+  cp ${component}.service /etc/systemd/system/ &>>${log}
+  echo $?
   echo -e "\e[33m<<<<<<Configuring nodeJS repos>>>>>>\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
   echo $?
@@ -8,9 +14,6 @@ func_nodeJS(){
   echo $?
   echo -e "\e[33m<<<<<<creating user roboshop>>>>>>\e[0m"
   useradd roboshop &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Copying ${component} file to systemd>>>>>>\e[0m"
-  cp ${component}.service /etc/systemd/system/ &>>${log}
   echo $?
   echo -e "\e[33m<<<<<<Removing if app directory exists>>>>>>\e[0m"
   rm -rf /app
@@ -30,20 +33,16 @@ func_nodeJS(){
   echo -e "\e[33m<<<<<<Installing npm modules>>>>>>\e[0m"
   npm install &>>${log}
   echo $?
-  echo -e "\e[33m<<<<<<restarting the services>>>>>>\e[0m" &>>${log}
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  echo $?
-  systemctl start ${component} &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Copying mongo repos to yum.repos.d>>>>>>\e[0m"
-  cp mongo.repo /etc/yum.repos.d/ &>>${log}
-  echo $?
   echo -e "\e[33m<<<<<<Installing mongodb shell >>>>>>\e[0m"
   yum install mongodb-org-shell -y &>>${log}
   echo $?
   echo -e "\e[33m<<<<<<Loading schema>>>>>>\e[0m"
-  mongo --host 172.31.41.3 < /app/schema/${component}.js &>>${log}
+  mongo --host mongodb.dineshdevops.com < /app/schema/${component}.js &>>${log}
+  echo $?
+  echo -e "\e[33m<<<<<<restarting the services>>>>>>\e[0m" &>>${log}
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl start ${component} &>>${log}
   echo $?
 }
 
