6e4c7d8eea9b21878e6884d77103f4b011b6e8dd
dineshdevops <dbramayanam@gmail.com>
2023-09-03 21:41:04 +0530

    fixed errors

--- 06-Cart.sh
+++ 06-Cart.sh
@@ -1,13 +1,3 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-cp cart.service /etc/systemd/system/
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable cart
-systemctl start cart
+component=user
+source common.sh
+func_nodeJS
