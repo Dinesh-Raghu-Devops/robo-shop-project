e7af6a355739136757184b27197a20d1981b63b9
dineshdevops <dbramayanam@gmail.com>
2023-09-03 18:30:58 +0530

    commited user.sh file

--- common.sh
+++ common.sh
@@ -1,49 +1,110 @@
 log=/tmp/logfile
-func_nodeJS(){
-  echo -e "\e[33m<<<<<<Copying mongo repos to yum.repos.d>>>>>>\e[0m"
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Copying ${component} file to systemd>>>>>>\e[0m"
-  cp ${component}.service /etc/systemd/system/ &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Configuring nodeJS repos>>>>>>\e[0m"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Installing NodeJS>>>>>>\e[0m"
-  yum install nodejs -y &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<creating user roboshop>>>>>>\e[0m"
-  useradd roboshop &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Removing if app directory exists>>>>>>\e[0m"
-  rm -rf /app
-  echo $?
-  echo -e "\e[33m<<<<<<Creating application directory>>>>>>\e[0m"
-  mkdir /app &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Insalling application>>>>>>\e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  echo $?
-  cd /app &>>${log}
-  echo $?
-  unzip /tmp/${component}.zip &>>${log}
-  echo $?
-  cd /app &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Installing npm modules>>>>>>\e[0m"
-  npm install &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Installing mongodb shell >>>>>>\e[0m"
-  yum install mongodb-org-shell -y &>>${log}
-  echo $?
-  echo -e "\e[33m<<<<<<Loading schema>>>>>>\e[0m"
-  mongo --host mongodb.dineshdevops.com < /app/schema/${component}.js &>>${log}
-  echo $?
+mongo_scheme=true
+func_service(){
   echo -e "\e[33m<<<<<<restarting the services>>>>>>\e[0m" &>>${log}
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
   systemctl start ${component} &>>${log}
   echo $?
 }
+func_nodeJS(){
+  echo -e "\e[33m<<<<<<Copying mongo repos to yum.repos.d>>>>>>\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  if [ $? -eq 0 ]; then
+    echo "SUCCESS"
+   else
+      echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Copying ${component} file to systemd>>>>>>\e[0m"
+  cp ${component}.service /etc/systemd/system/ &>>${log}
+  if [ $? -eq 0 ]; then
+      echo "SUCCESS"
+     else
+      echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Configuring nodeJS repos>>>>>>\e[0m"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  if [ $? -eq 0 ]; then
+      echo "SUCCESS"
+     else
+      echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Installing NodeJS>>>>>>\e[0m"
+  yum install nodejs -y &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<creating user roboshop>>>>>>\e[0m"
+  useradd roboshop &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Removing if app directory exists>>>>>>\e[0m"
+  rm -rf /app
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Creating application directory>>>>>>\e[0m"
+  mkdir /app &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+      else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Insalling application>>>>>>\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  cd /app &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  unzip /tmp/${component}.zip &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  cd /app &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Installing npm modules>>>>>>\e[0m"
+  npm install &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Installing mongodb shell >>>>>>\e[0m"
+  yum install mongodb-org-shell -y &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  echo -e "\e[33m<<<<<<Loading schema>>>>>>\e[0m"
+  mongo --host mongodb.dineshdevops.com < /app/schema/${component}.js &>>${log}
+  if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+       else
+        echo "FAILED"
+  fi
+  func_service
+}
+
 
 
